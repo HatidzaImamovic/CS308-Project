0b8f7b5cd69f3b327ee20ccce0b499978d409d93
Hatidza <hatidza12.imamovic12@gmail.com>
2026-05-04 15:13:21 +0200

    add image

--- frontend/mobile/screens/LoginScreen.js
+++ frontend/mobile/screens/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, Image } from 'react-native';
 
 export default function LoginScreen({ navigation }) {
   const [username, setUsername] = useState('');
@@ -27,18 +27,18 @@ export default function LoginScreen({ navigation }) {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Centrometal</Text>
-      <TextInput style={styles.input} placeholder="Username" value={username} onChangeText={setUsername} />
-      <TextInput style={styles.input} placeholder="Password" value={password} onChangeText={setPassword} secureTextEntry />
+      <Image source={require('../public/centrometalLogo.png')} style={styles.logo} />
+      <TextInput style={styles.input} placeholder="Ime korisnika" value={username} onChangeText={setUsername} />
+      <TextInput style={styles.input} placeholder="Šifra" value={password} onChangeText={setPassword} secureTextEntry />
       {error ? <Text style={styles.error}>{error}</Text> : null}
-      <Button title="Login" onPress={handleLogin} />
+      <Button title="Prijavi se" onPress={handleLogin} color='#1ca8b2' />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: 'center', padding: 20 },
-  title: { fontSize: 24, fontWeight: 'bold', marginBottom: 20, textAlign: 'center' },
-  input: { borderWidth: 1, borderColor: '#ccc', padding: 10, marginBottom: 10, borderRadius: 5, autoCapitalize="none" },
+  logo: { width: 300, height: 100, marginBottom: 20, alignSelf: 'center',  resizeMode: 'contain', marginTop: -150, },
+  input: { borderWidth: 1, borderColor: '#ccc', padding: 10, marginBottom: 10, borderRadius: 5 },
   error: { color: 'red', marginBottom: 10 },
 });
