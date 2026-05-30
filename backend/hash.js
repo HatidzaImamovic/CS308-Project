8ca220cc3a29a7547a457f1b4cc31e9d3ba10709
Esma Đuherić <esmadjuheric04@gmail.com>
2026-05-31 00:36:48 +0200

    Remove hash.js

--- backend/hash.js
+++
@@ -1,7 +0,0 @@
- const bcrypt = require("bcrypt");
-async function generateHash() {
-    const password = "mypassword123";
-    const hash = await bcrypt.hash(password, 10);
-    console.log(hash);
-}
-generateHash();
